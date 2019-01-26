bb328f97666e2c73acbb74a6d78f8801ca9f1454
Gérald Döserich <github@doeseri.ch>
2019-01-26 16:40:58 +0100

    Added comments

--- DeploymentToolkit.Messaging/MessageID.cs
+++ DeploymentToolkit.Messaging/MessageID.cs
@@ -2,24 +2,42 @@
 {
     public enum MessageId : byte
     {
+        // Client = TrayApp
+        // Server = DeploymentToolkit executable
+        // IFEObl = Image File Execution Options Blocker
+
+        // Server <-  Client
         InitialConnectMessage,
+        // Server  -> Client
         DeploymentInformationMessage,
 
+        // Server  -> Client
         InstallationStarted,
+        // Server  -> Client
         InstallationEnded,
+        // Server  -> Client
         InstallationError,
 
+        // Server  -> Client
         UninstallationStarted,
+        // Server  -> Client
         UninstallationEnded,
+        // Server  -> Client
         UninstallationError,
 
+        // IFEObl  -> Server
+        // Server  -> Client
         ExecutionBlocked,
 
+        // Server  -> Client
         ShowBalloonTip,
 
+        // Server <-  Client
         ContinueDeployment,
 
+        // Server  -> Client
         DeferDeployment,
+        // Server  -> Client
         CloseApplications
     }
 }
