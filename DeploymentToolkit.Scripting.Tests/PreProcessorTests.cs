19308bf0d70502bd32819da421af400119e8d6e6
Gérald Döserich <github@doeseri.ch>
2019-02-16 11:48:39 +0100

    Added Preprocessor to handle functions and variables. Added Preprocessor tests.

---
+++ DeploymentToolkit.Scripting.Tests/PreProcessorTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace DeploymentToolkit.Scripting.Tests
+{
+    [TestClass()]
+    public class PreProcessorTests
+    {
+        [TestMethod()]
+        public void ProcessTest()
+        {
+            var conditions = new List<ExpectedConditon>()
+            {
+                new ExpectedConditon()
+                {
+                    Condition = "$Is64Bit$"
+                },
+                new ExpectedConditon()
+                {
+                    Condition = "$Is32Bit$"
+                },
+                new ExpectedConditon()
+                {
+                    Condition = "$DT_InstallPath$"
+                },
+                new ExpectedConditon()
+                {
+                    Condition = "$DT_FilesPath$"
+                },
+                new ExpectedConditon()
+                {
+                    Condition = "$DT_IsTaskSequence$"
+                },
+            };
+
+            foreach(var condition in conditions)
+            {
+                Assert.AreNotEqual(condition.Condition, PreProcessor.Process(condition.Condition));
+            }
+        }
+
+        [TestMethod()]
+        public void ProcessTestFunctions()
+        {
+            var conditions = new List<ExpectedConditon>()
+            {
+                new ExpectedConditon()
+                {
+                    Condition = @"$FileExists(C:\Windows\explorer.exe)$"
+                },
+                new ExpectedConditon()
+                {
+                    Condition = @"$DirectoryExists(C:\Windows)$"
+                },
+            };
+
+            foreach (var condition in conditions)
+            {
+                Assert.AreNotEqual(condition.Condition, PreProcessor.Process(condition.Condition));
+            }
+        }
+    }
+}
---
+++ DeploymentToolkit.Scripting/PreProcessor.cs
@@ -0,0 +1,192 @@
+﻿using System.Diagnostics;
+using System.Collections.Generic;
+using System;
+using DeploymentToolkit.DTEnvironment;
+using DeploymentToolkit.DTEnvironment.Exceptions;
+using System.IO;
+
+namespace DeploymentToolkit.Scripting
+{
+    public static class BoolExtension
+    {
+        public static string ToIntString(this bool boolean)
+        {
+            return Convert.ToInt32(boolean).ToString();
+        }
+    }
+
+    public static class PreProcessor
+    {
+        private const string _separator = "$";
+
+        private static readonly Dictionary<string, Func<string>> _variables = new Dictionary<string, Func<string>>()
+        {
+            {
+                "Is64Bit",
+                delegate()
+                {
+                    return Environment.Is64BitOperatingSystem.ToIntString();
+                }
+            },
+            {
+                "Is64BitProcess",
+                delegate()
+                {
+                    return Environment.Is64BitProcess.ToIntString();
+                }
+            },
+            {
+                "Is32Bit",
+                delegate()
+                {
+                    return (!Environment.Is64BitOperatingSystem).ToIntString();
+                }
+            },
+            {
+                "Is32BitProcess",
+                delegate()
+                {
+                    return (!Environment.Is64BitProcess).ToIntString();
+                }
+            },
+            {
+                "DT_InstallPath",
+                delegate()
+                {
+                    try
+                    {
+                        return EnvironmentVariables.DeploymentToolkitInstallPath;
+                    }
+                    catch(DeploymentToolkitInstallPathNotFoundException)
+                    {
+                        return "";
+                    }
+                }
+            },
+            {
+                "DT_FilesPath",
+                delegate()
+                {
+                    // TODO: Add the right path
+                    try
+                    {
+                        return EnvironmentVariables.DeploymentToolkitInstallPath;
+                    }
+                    catch(DeploymentToolkitInstallPathNotFoundException)
+                    {
+                        return "";
+                    }
+                }
+            },
+            {
+                "DT_IsTaskSequence",
+                delegate()
+                {
+                    return EnvironmentVariables.IsRunningInTaskSequence.ToIntString();
+                }
+            },
+        };
+
+        private static readonly Dictionary<string, Func<string[], string>> _functions = new Dictionary<string, Func<string[], string>>()
+        {
+            {
+                "FileExists",
+                delegate(string[] parameters)
+                {
+                    if(parameters.Length == 0)
+                        return false.ToIntString();
+
+                    var path = parameters[0];
+                    if(!Path.IsPathRooted(path))
+                        path = Path.GetFullPath(path);
+                    return File.Exists(path).ToIntString();
+                }
+            },
+            {
+                "DirectoryExists",
+                delegate(string[] parameters)
+                {
+                    if(parameters.Length == 0)
+                        return false.ToIntString();
+
+                    var path = parameters[0];
+                    if(!Path.IsPathRooted(path))
+                        path = Path.GetFullPath(path);
+                    return Directory.Exists(path).ToIntString();
+                }
+            }
+        };
+
+        public static string Process(string data)
+        {
+            // Nothing to process
+            if (!data.Contains(_separator))
+                return data;
+
+            var processed = data;
+            var toProcess = data;
+
+            do
+            {
+                var start = toProcess.IndexOf(_separator);
+                if (start == -1)
+                    break;
+
+                var part = toProcess.Substring(start + 1, toProcess.Length - start - 1);
+                var end = part.IndexOf(_separator);
+                if (end == -1)
+                    break;
+
+                var variableName = part.Substring(start, end);
+                Debug.WriteLine($"Found {variableName}");
+                Debug.WriteLine($"ToProcess: {toProcess}");
+
+                if (variableName.Contains("("))
+                {
+                    var variablesStart = variableName.IndexOf('(');
+                    var variablesEnd = variableName.IndexOf(')');
+
+                    if (variablesStart == -1 || variablesEnd == -1)
+                    {
+                        processed = processed.Replace($"${variableName}$", "INVALID PARAMETERS");
+                        continue;
+                    }
+
+                    var parameterString = variableName.Substring(variablesStart, variablesEnd - variablesStart).TrimStart('(').TrimEnd(')');
+                    if(string.IsNullOrEmpty(parameterString))
+                    {
+                        processed = processed.Replace($"${variableName}$", "INVALID PARAMETERS");
+                        continue;
+                    }
+
+                    var functionName = variableName.Substring(0, variablesStart);
+                    Debug.WriteLine($"Function: {functionName}");
+                    Debug.WriteLine($"Params: {parameterString}");
+
+                    if (_functions.ContainsKey(functionName))
+                    {
+                        var parameters = parameterString.Split(',');
+                        processed = processed.Replace($"${variableName}$", _functions[functionName].Invoke(parameters));
+                    }
+                    else
+                    {
+                        processed = processed.Replace($"${variableName}$", "INVALID FUNCTION");
+                    }
+                }
+                else
+                {
+                    if (_variables.ContainsKey(variableName))
+                        processed = processed.Replace($"${variableName}$", _variables[variableName].Invoke());
+                    else
+                        processed = processed.Replace($"${variableName}$", "NOT FOUND");
+                }
+                toProcess = toProcess.Substring(end, toProcess.Length - end);
+            }
+            while (toProcess.Contains(_separator));
+
+            Debug.WriteLine($"Processed: {processed}");
+
+            return processed;
+        }
+    }
+}
