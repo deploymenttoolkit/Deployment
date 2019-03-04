ec871bdd2f54e029f2beb83efdc4c9ffa8fba91d
Gérald Döserich <github@doeseri.ch>
2019-03-04 13:33:44 +0100

    Added DeploymentToolkit.Registry to better handle 32 bit and 64 bit registry. Added tests for DeploymentToolkit.Registry

---
+++ DeploymentToolkit.Registry/Modals/WinRegistryKey.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.Win32;
+using NLog;
+using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace DeploymentToolkit.Registry.Modals
+{
+    public class WinRegistryKey : IDisposable
+    {
+        [DllImport("advapi32.dll")]
+        static extern int RegQueryValueEx(
+            UIntPtr hKey,
+            string lpValueName,
+            int lpReserved,
+            ref RegistryValueKind lpType,
+            IntPtr lpData,
+            ref int lpcbData
+        );
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        static extern int RegSetValueEx(
+            UIntPtr hKey,
+            [MarshalAs(UnmanagedType.LPStr)] string lpValueName,
+            int Reserved,
+            RegistryValueKind dwType,
+            IntPtr lpData,
+            int cbData
+        );
+
+        [DllImport("advapi32.dll")]
+        static extern int RegDeleteValue(
+            UIntPtr hKey,
+            [MarshalAs(UnmanagedType.VBByRefStr)] ref string lpValueName
+        );
+
+        public string Key { get; }
+
+        internal UIntPtr RegPointer { get; }
+
+        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+        private WinRegistryBase _winRegistryBase { get; }
+
+        public WinRegistryKey(WinRegistryBase winRegistryBase, UIntPtr regPointer, string Key)
+        {
+            this._winRegistryBase = winRegistryBase;
+            this.Key = Key;
+            this.RegPointer = regPointer;
+        }
+
+        public bool DeleteValue(string key)
+        {
+            _logger.Trace($"DeleteValue({key})");
+            var error = RegDeleteValue(RegPointer, ref key);
+            _logger.Trace($"Errorlevel: {error}");
+
+            if (error == 0)
+                return true;
+
+            throw new Win32Exception(error);
+        }
+
+        public bool SetValue(string key, object value, RegistryValueKind type)
+        {
+            _logger.Trace($"SetValue({key}, {value}, {type})");
+            var size = 0;
+            var data = IntPtr.Zero;
+            try
+            {
+                switch (type)
+                {
+                    case RegistryValueKind.String:
+                        {
+                            _logger.Trace("Allocating string ...");
+                            size = ((string)value).Length + 1;
+                            data = Marshal.StringToHGlobalAnsi((string)value);
+                        }
+                        break;
+
+                    case RegistryValueKind.DWord:
+                        {
+                            _logger.Trace("Allocating int ...");
+                            size = Marshal.SizeOf(typeof(Int32));
+                            data = Marshal.AllocHGlobal(size);
+                            Marshal.WriteInt32(data, (int)value);
+                        }
+                        break;
+
+                    case RegistryValueKind.QWord:
+                        {
+                            _logger.Trace("Allocating long ...");
+                            size = Marshal.SizeOf(typeof(Int64));
+                            data = Marshal.AllocHGlobal(size);
+                            Marshal.WriteInt64(data, (int)value);
+                        }
+                        break;
+                }
+
+                var error = RegSetValueEx(RegPointer, key, 0, type, data, size);
+                _logger.Trace($"Errorlevel: {error}");
+
+                if (error == 0)
+                        return true;
+
+                throw new Win32Exception(error);
+            }
+            finally
+            {
+                if (data != IntPtr.Zero)
+                    Marshal.FreeHGlobal(data);
+            }
+        }
+
+        public T GetValue<T>(string key, RegistryValueKind type)
+        {
+            _logger.Trace($"GetValue({key}, {type})");
+            int size = 0;
+            var result = IntPtr.Zero;
+            try
+            {
+                var error = RegQueryValueEx(RegPointer, key, 0, ref type, IntPtr.Zero, ref size);
+                _logger.Trace($"Errorlevel: {error}");
+                if (error != 0)
+                    throw new Win32Exception(error);
+
+                result = Marshal.AllocHGlobal(size);
+                error = RegQueryValueEx(RegPointer, key, 0, ref type, result, ref size);
+                _logger.Trace($"Errorlevel: {error}");
+                if (error == 0)
+                {
+                    var resultObject = default(T);
+                    switch (type)
+                    {
+                        case RegistryValueKind.String:
+                            _logger.Trace("Fetching string ...");
+                            resultObject = (T)Convert.ChangeType(Marshal.PtrToStringAnsi(result), typeof(T));
+                            break;
+                        case RegistryValueKind.DWord:
+                            _logger.Trace("Fetching int ...");
+                            resultObject = (T)Convert.ChangeType(Marshal.ReadInt32(result), typeof(T));
+                            break;
+                        case RegistryValueKind.QWord:
+                            _logger.Trace("Fetching long ...");
+                            resultObject = (T)Convert.ChangeType(Marshal.ReadInt64(result), typeof(T));
+                            break;
+
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(type), "Unsupported Reg type");
+                    }
+
+                    return resultObject;
+                }
+                throw new Win32Exception(error);
+            }
+            finally
+            {
+                if (result != IntPtr.Zero)
+                    Marshal.FreeHGlobal(result);
+            }
+        }
+
+        public void Dispose()
+        {
+            _winRegistryBase.CloseKey(this);
+        }
+    }
+}
---
+++ DeploymentToolkit.Registry/WinRegistryBase.cs
@@ -0,0 +1,100 @@
+﻿using DeploymentToolkit.Registry.Modals;
+using Microsoft.Win32;
+using NLog;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace DeploymentToolkit.Registry
+{
+    public abstract class WinRegistryBase
+    {
+        [DllImport("advapi32.dll")]
+        static extern int RegOpenKeyEx(
+            RegistryHive hKey,
+            [MarshalAs(UnmanagedType.VBByRefStr)] ref string subKey,
+            int options,
+            RegAccess sam,
+            out UIntPtr phkResult
+        );
+
+        [DllImport("advapi32.dll")]
+        static extern int RegCloseKey(
+            UIntPtr hKey
+        );
+
+        [DllImport("advapi32.dll")]
+        static extern int RegCreateKeyEx(
+            UIntPtr hKey,
+            [MarshalAs(UnmanagedType.VBByRefStr)] ref string lpSubKey,
+            uint Reserved,
+            string lpClass,
+            RegOption dwOptions,
+            RegSAM samDesired,
+            ref SECURITY_ATTRIBUTES lpSecurityAttributes,
+            out UIntPtr phkResult,
+            out RegDisposition lpdwDisposition
+        );
+
+        protected abstract RegAccess RegAccess { get; }
+
+        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public WinRegistryKey OpenKey(string path, bool write = false)
+        {
+            _logger.Trace($"OpenKey({path}, {write})");
+            if (string.IsNullOrEmpty(path))
+                throw new ArgumentNullException(nameof(path));
+
+            var hive = GetHiveFromString(path, out var newPath);
+            _logger.Trace($"Hive: {hive}");
+            var error = RegOpenKeyEx(hive, ref newPath, 0, write ? RegAccess.KEY_ALL_ACCESS : RegAccess.KEY_READ | RegAccess, out var key);
+            _logger.Trace($"Errorlevel: {error}");
+
+            if (error == 0)
+                return new WinRegistryKey(this, key, newPath);
+            throw new Win32Exception(error);
+        }
+
+        public bool CloseKey(WinRegistryKey key)
+        {
+            _logger.Trace($"CloseKey({key.Key})");
+            var error = RegCloseKey(key.RegPointer);
+            _logger.Trace($"Errorlevel: {error}");
+
+            if (error == 0)
+                return true;
+            throw new Win32Exception(error);
+        }
+
+        private RegistryHive GetHiveFromString(string path, out string newPath)
+        {
+            _logger.Trace($"GetHiveFromString({path})");
+            var split = path.Split('\\');
+            if (split.Length == 0)
+                throw new ArgumentOutOfRangeException(nameof(path));
+
+            var hive = split[0].ToUpper();
+            _logger.Trace($"Hive: {hive}");
+            newPath = split.Length > 2 ? split.Skip(1).Aggregate((i, j) => i + "\\" + j) : split[1];
+            _logger.Trace($"NewPath: {newPath}");
+            switch(hive)
+            {
+                case "HKEY_LOCAL_MACHINE":
+                    return RegistryHive.LocalMachine;
+                case "HKEY_CURRENT_USER":
+                    return RegistryHive.CurrentUser;
+                case "HKEY_CURRENT_CONFIG":
+                    return RegistryHive.CurrentConfig;
+                case "HKEY_CLASSES_ROOT":
+                    return RegistryHive.ClassesRoot;
+                case "HKEY_USERS":
+                    return RegistryHive.Users;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(path), "Invalid RegHive");
+            }
+        }
+    }
+}
