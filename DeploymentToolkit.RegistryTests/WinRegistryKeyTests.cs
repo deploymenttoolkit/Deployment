2b6c88d829e1a61f2d7ac26bae3eb9f4496a976f
Gérald Döserich <github@doeseri.ch>
2019-03-04 14:32:10 +0100

    Removed unused references

--- DeploymentToolkit.RegistryTests/WinRegistryKeyTests.cs
+++ DeploymentToolkit.RegistryTests/WinRegistryKeyTests.cs
@@ -1,11 +1,6 @@
 ﻿using DeploymentToolkit.Registry;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DeploymentToolkit.RegistryTests
 {
