63d48407d84af3f5a8ed537154c82d9ec07e0ed3
Gérald Döserich <github@doeseri.ch>
2019-03-06 21:12:20 +0100

    Made DeploymentToolkit.Registry obsolet with the using of DeploymentToolkit.RegistryWrapper

---
+++ DeploymentToolkit.RegistryWrapper/WinRegistryBase.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Win32;
+
+namespace DeploymentToolkit.RegistryWrapper
+{
+    public abstract class WinRegistryBase
+    {
+        public abstract RegistryView View { get; }
+
+        internal RegistryKey HKEY_LOCAL_MACHINE
+        {
+            get
+            {
+                return RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, View);
+            }
+        }
+
+        internal RegistryKey HKEY_USERS
+        {
+            get
+            {
+                return RegistryKey.OpenBaseKey(RegistryHive.Users, View);
+            }
+        }
+
+        internal RegistryKey HKEY_CLASSES_ROOT
+        {
+            get
+            {
+                return RegistryKey.OpenBaseKey(RegistryHive.ClassesRoot, View);
+            }
+        }
+
+        internal RegistryKey HKEY_CURRENT_CONFIG
+        {
+            get
+            {
+                return RegistryKey.OpenBaseKey(RegistryHive.CurrentConfig, View);
+            }
+        }
+
+        internal RegistryKey HKEY_CURRENT_USER
+        {
+            get
+            {
+                return RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, View);
+            }
+        }
+    }
+}
