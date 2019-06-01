caabaeb3b4c819f42b4d8d2517bd66bf65e253d1
Gérald Döserich <github@doeseri.ch>
2019-06-01 11:21:44 +0200

    Added DeploymentToolkit.Restart to have a seperate exe to spawn for restart

---
+++ DeploymentToolkit.Restart/Program.cs
@@ -0,0 +1,22 @@
+﻿using DeploymentToolkit.Utils;
+using System;
+using System.Threading;
+
+namespace DeploymentToolkit.Restart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Restarting in 10 Seconds ...");
+            for(var i = 10; i != 0; i--)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Restarting ...");
+
+            PowerUtil.Restart();
+        }
+    }
+}
