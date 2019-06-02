0dc32fe72c797ff37a8767daed41cf818917b06b
Gérald Döserich <github@doeseri.ch>
2019-06-02 11:26:01 +0200

    Removed not used usings

--- DeploymentToolkit.ToolkitEnvironmentTests/RegistryManagerTests.cs
+++ DeploymentToolkit.ToolkitEnvironmentTests/RegistryManagerTests.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using DeploymentToolkit.ToolkitEnvironment;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics;
 
 namespace DeploymentToolkit.ToolkitEnvironment.Tests
