0afd04488c5b194af589363fd748ddcf4240218d
Gérald Döserich <github@doeseri.ch>
2019-06-05 15:27:51 +0200

    Fixed IsElevated not working in domain environments by adding second check method

--- DeploymentToolkit.Environment/Administrator.cs
+++ DeploymentToolkit.Environment/Administrator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Security.Principal;
@@ -21,9 +22,22 @@ namespace DeploymentToolkit.ToolkitEnvironment
         #endregion
 
         #region Native-Variables
-        private static uint STANDARD_RIGHTS_READ = 0x00020000;
-        private static uint TOKEN_QUERY = 0x0008;
-        private static uint TOKEN_READ = (STANDARD_RIGHTS_READ | TOKEN_QUERY);
+        public const UInt32 STANDARD_RIGHTS_REQUIRED = 0x000F0000;
+        public const UInt32 STANDARD_RIGHTS_READ = 0x00020000;
+        public const UInt32 TOKEN_ASSIGN_PRIMARY = 0x0001;
+        public const UInt32 TOKEN_DUPLICATE = 0x0002;
+        public const UInt32 TOKEN_IMPERSONATE = 0x0004;
+        public const UInt32 TOKEN_QUERY = 0x0008;
+        public const UInt32 TOKEN_QUERY_SOURCE = 0x0010;
+        public const UInt32 TOKEN_ADJUST_PRIVILEGES = 0x0020;
+        public const UInt32 TOKEN_ADJUST_GROUPS = 0x0040;
+        public const UInt32 TOKEN_ADJUST_DEFAULT = 0x0080;
+        public const UInt32 TOKEN_ADJUST_SESSIONID = 0x0100;
+        public const UInt32 TOKEN_READ = (STANDARD_RIGHTS_READ | TOKEN_QUERY);
+        public const UInt32 TOKEN_ALL_ACCESS = (STANDARD_RIGHTS_REQUIRED | TOKEN_ASSIGN_PRIMARY |
+            TOKEN_DUPLICATE | TOKEN_IMPERSONATE | TOKEN_QUERY | TOKEN_QUERY_SOURCE |
+            TOKEN_ADJUST_PRIVILEGES | TOKEN_ADJUST_GROUPS | TOKEN_ADJUST_DEFAULT |
+            TOKEN_ADJUST_SESSIONID);
 
         public enum TOKEN_INFORMATION_CLASS
         {
@@ -89,14 +103,29 @@ namespace DeploymentToolkit.ToolkitEnvironment
                 if (_isElevated.HasValue)
                     return _isElevated.Value;
 
+                var tokenHandle = IntPtr.Zero;
                 try
                 {
-                    var tokenHandle = IntPtr.Zero;
-                    if (!OpenProcessToken(Process.GetCurrentProcess().Handle, TOKEN_READ, out tokenHandle))
+                    if (!OpenProcessToken(Process.GetCurrentProcess().Handle, TOKEN_ALL_ACCESS, out tokenHandle))
                     {
                         _logger.Error($"Failed to get process token. Win32 error: {Marshal.GetLastWin32Error()}");
                         _isElevated = false;
                         return false;
+                    }
+
+                    try
+                    {
+                        var identity = new WindowsIdentity(tokenHandle);
+                        var principal = new WindowsPrincipal(identity);
+
+                        _isElevated = principal.IsInRole(WindowsBuiltInRole.Administrator) || principal.IsInRole(0x200);
+
+                        if (_isElevated.Value)
+                            return true;
+                    }
+                    catch(Exception ex)
+                    {
+                        _logger.Debug(ex, "Failed to check for elevation. Using method 2");
                     }
 
                     try
@@ -135,11 +164,6 @@ namespace DeploymentToolkit.ToolkitEnvironment
                     {
                         _logger.Error(ex);
                     }
-                    finally
-                    {
-                        if (tokenHandle != IntPtr.Zero)
-                            CloseHandle(tokenHandle);
-                    }
 
                     _isElevated = false;
                     return false;
@@ -148,6 +172,11 @@ namespace DeploymentToolkit.ToolkitEnvironment
                 {
                     _logger.Error(ex, "Failed to process IsElevated");
                     return false;
+                }
+                finally
+                {
+                    if (tokenHandle != IntPtr.Zero)
+                        CloseHandle(tokenHandle);
                 }
             }
         }
