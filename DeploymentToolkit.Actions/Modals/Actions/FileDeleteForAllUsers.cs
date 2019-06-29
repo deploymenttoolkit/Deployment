4c42869f073095e275d6c9d3b95877e5f616e52f
Gérald Döserich <github@doeseri.ch>
2019-06-30 00:06:17 +0200

    Added File*ForAllUsers actions to xml

---
+++ DeploymentToolkit.Actions/Modals/Actions/FileDeleteForAllUsers.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml.Serialization;
+
+namespace DeploymentToolkit.Actions.Modals.Actions
+{
+
+    [XmlRoot(ElementName = "FileDeleteForAllUsers")]
+    public class FileDeleteForAllUsers : IExecutableAction
+    {
+        [XmlAttribute(AttributeName = "Target")]
+        public string Target { get; set; }
+
+        [XmlAttribute(AttributeName = "IncludeDefaultProfile")]
+        public bool IncludeDefaultProfile { get; set; }
+        [XmlAttribute(AttributeName = "IncludePublicProfile")]
+        public bool IncludePublicProfile { get; set; }
+
+        public bool Execute()
+        {
+            return FileActions.DeleteFileForAllUsers(Target, IncludeDefaultProfile, IncludePublicProfile);
+        }
+    }
+}
