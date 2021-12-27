170f89deafe7b9806c7ee2ce9073250972181f81
Gérald Döserich <github@doeseri.ch>
2021-12-27 17:28:19 +0100

    Updated modals to reflect TrayApp WinForms to WPF switch

---
+++ DeploymentToolkit.Modals/Settings/Tray/DefaultButtonSettings.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Serialization;
+
+namespace DeploymentToolkit.Modals.Settings.Tray
+{
+    public class DefaultButtonSettings : IButtonSettings
+    {
+        [XmlElement(ElementName = "DefaultHeight", IsNullable = true)]
+        public int? Height { get; set; } = 30;
+        [XmlElement(ElementName = "DefaultWidth", IsNullable = true)]
+        public int? Width { get; set; } = 120;
+        [XmlElement(ElementName = "DefaultFontColor", IsNullable = true)]
+        public string FontColor { get; set; } = string.Empty;
+        [XmlElement(ElementName = "DefaultFontWeight", IsNullable = true)]
+        public string FontWeight { get; set; } = string.Empty;
+        [XmlElement(ElementName = "DefaultBackgroundColor", IsNullable = true)]
+        public string BackgroundColor { get; set; } = "#F1F1F1";
+        [XmlElement(ElementName = "DefaultBorderColor", IsNullable = true)]
+        public string BorderColor { get; set; }
+
+        public ButtonSettings UpgradeButton { get; set; }
+        public ButtonSettings ScheduleButton { get; set; }
+        public ButtonSettings MinimizeButton { get; set; }
+    }
+}
--- DeploymentToolkit.Modals/Settings/TrayAppSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DeploymentToolkit.Modals.Settings
-{
-    public class TrayAppSettings
-    {
-        public bool EnableAppList { get; set; }
-        public string BackgroundColor { get; set; } = string.Empty;
-        public string ForegroundColor { get; set; } = string.Empty;
-    }
-}
